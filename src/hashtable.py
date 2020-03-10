a465717e329ea97090ca62b205c6344cadb78182
Temitope Akinsoto <takinsoto@gmail.com>
2020-03-10 10:10:34 +0100

    mvp

--- src/hashtable.py
+++ src/hashtable.py
@@ -9,30 +9,43 @@ class LinkedPair:
 
 class HashTable:
     '''
-    A hash table that with `capacity` buckets
+    A hash table that with `capacity` storage
     that accepts string keys
     '''
     def __init__(self, capacity):
-        self.capacity = capacity  # Number of buckets in the hash table
+        self.capacity = capacity  # Number of storage in the hash table
         self.storage = [None] * capacity
+        self.count = 0 
 
-
+  
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
+        
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
+        start = time.time()
+        hash(key)
+        end = time.time()
         return hash(key)
+        
 
 
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
+      #start from an arbitrary prime number 
+      hash = 9491 
+      #iterate over each character in the key 
+      for el in key: 
+        #set hash value to the bit shift left by 5 
+        #of the hash value and sum of the hash value
+        #then add the value for the char
+        hash = ((hash << 5) + hash) + ord(el)
+        #return the hash value
+        return hash
 
 
     def _hash_mod(self, key):
@@ -41,50 +54,96 @@ class HashTable:
         within the storage capacity of the hash table.
         '''
         return self._hash(key) % self.capacity
+      
 
 
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
-        pass
+        self.count += 1 
+        #compute index of key using hash function
+        index = self._hash(key)
+        #create new node if bucket at index is empty 
+        node = self.storage[index]        
+        if node is None: 
+          self.storage[index] = LinkedPair(key, value)
+          return 
+        
+        prev = node 
+        while node is not None: 
+          #collision, a node exists at this index
+          #iterate to the end of the list and add new node 
+          #with provided  key / value 
+          prev = node 
+          node = node.next #iteration
+          prev.next = LinkedPair(key, value)
 
 
 
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
-        pass
+        #compute index of key using a hash function 
+        index = self._hash_mod(key)
+        node = self.storage[index]
+        prev = None
+        
+        #iterate to the requested node 
+        while node is not None and node.key != key: 
+          prev = node
+          node = node.next 
+          
+        if node is None:
+          print("warning! key is not found")
+        
+        else: 
+          self.count -= 1 
+          result = node.value
+          if prev is None:
+            self.storage[index]  = node.next 
+          else: 
+            prev.next = prev.next.next
+            
+          return result
 
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        node = self.storage[index]
+        while node is not None and node.key != key: 
+          node = node.next 
+        if node is None: 
+          return None
+        else: 
+          return node.value
 
 
     def resize(self):
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Fill this in.
         '''
         pass
+
+    def __resize__(self):
+      self.capacity *= 2
+      new_storage = [None] * self.capacity
+      for i in range(self.count):
+         new_storage[i] = self.storage[i]
+      self.storage = new_storage 
+      
 
 
 
